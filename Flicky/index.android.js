208f80f05a0db93d6485d35f313ea61671bcffd7
Bart Chrzaszcz <bart.chr@gmail.com>
2016-12-31 01:46:28 -0500

    Implemented sliders, added buttons for app customization

--- Flicky/index.android.js
+++ Flicky/index.android.js
@@ -14,45 +14,54 @@ var Slider  = require('react-native-slider');
 var window  = require('Dimensions').get('window');
 
 
-var Flicky = React.createClass({
+export default class Flicky extends Component {
 
-  getInitialState: function() {
-    return {
+  constructor(props) {
+    super(props);
+    this.openModal3 = this.openModal3.bind(this);
+
+    this.state = {
       isOpen: false,
       isDisabled: false,
       swipeToClose: false,
-      sliderValueX1: 0.3,
-      sliderValueX2: 0.3,
-      sliderValueY1: 0.3,
-      sliderValueY2: 0.3,
-      sliderValueZ1: 0.3,
-      sliderValueZ2: 0.3,
+      sliderValueUp: 0.3,
+      sliderValueDown: 0.3,
+      sliderValueForward: 0.3,
+      sliderValueBackward: 0.3,
+      sliderValueLeft: 0.3,
+      sliderValueRight: 0.3,
+      appUp: 'upper',
+      appDown: 'downer',
+      appForward: "forwarder",
+      appBackward: "backwarder",
+      appLeft: "lefter",
+      appRight: "Righter"
     };
-  },
+  }
 
-  openModal3: function(id) {
+  openModal3(id) {
     this.refs.modal3.open();
-  },
+  }
 
-  toggleDisable: function() {
+  toggleDisable() {
     this.setState({isDisabled: !this.state.isDisabled});
-  },
+  }
 
-  toggleSwipeToClose: function() {
+  toggleSwipeToClose() {
     this.setState({swipeToClose: !this.state.swipeToClose});
-  },
+  }
 
-  onClose: function() {
+  onClose() {
     console.log('Modal just closed');
-  },
+  }
 
-  onOpen: function() {
+  onOpen() {
     console.log('Modal just openned');
-  },
+  }
 
-  onClosingState: function(state) {
+  onClosingState(state) {
     console.log('the open/close of the swipeToClose just changed');
-  },
+  }
 
   renderList() {
     var list = [];
@@ -62,9 +71,9 @@ var Flicky = React.createClass({
     }
 
     return list;
-  },
+  }
 
-  render: function() {
+  render() {
     return (
       <View style={styles.container}>
         <Toolbar
@@ -72,35 +81,54 @@ var Flicky = React.createClass({
           centerElement="Title"
           rightElement={<Text>You can put here whatever you want</Text>}
         />
-         <Button style={styles.buttons} value="Sensitivity" onPress={this.openModal3} />
-          <Divider />
-         <Button style={styles.buttons} value="Direction Customization" onPress={()=> console.log("I pressed a flat button")} />
+        <Button text="Sensitivity" style={styles.buttons} value="Sensitivity" onPress={this.openModal3} />
+        <Divider />
+        <Button text="Direction Customization" style={styles.buttons} value="Direction Customization" onPress={()=> console.log("I pressed a flat button")} />
+
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-end', backgroundColor: 'green', paddingBottom: 20}}><Text style={{textAlign: 'center', backgroundColor: 'yellow'}}>Up</Text></View>
+        <View style={{flex: 2, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 20, backgroundColor: 'orange'}}>
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{backgroundColor:'red'}}>Left</Text>
+          </View>
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{paddingBottom: 20}}>
+              <Text style={{backgroundColor:'red'}}>Forward</Text>
+            </View>
+            <View style={{paddingTop: 20}}>
+              <Text style={{backgroundColor:'red'}}>Backward</Text>
+            </View>
+          </View>
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{backgroundColor:'red'}}>Right</Text>
+          </View>
+        </View>
+        <View style={{flex: 2, justifyContent: 'flex-start', alignItems: 'center', height: 20, backgroundColor: 'pink', paddingTop: 20}}><Text style={{backgroundColor: 'purple'}}>Down</Text></View>
 
 
          <Modal style={[styles.modal, styles.modal3]} position={"center"} ref={"modal3"} isDisabled={this.state.isDisabled}>
-            <Text style={styles.text}>X1</Text>
-            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueX1} onValueChange={(value) => this.setState({sliderValueX1: value})} />
+            <Text style={styles.text}>Up</Text>
+            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueUp} onValueChange={(value) => this.setState({sliderValueUp: value})} />
 
-            <Text style={styles.text}>X2</Text>
-            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueX2} onValueChange={(value) => this.setState({sliderValueX2: value})} />
+            <Text style={styles.text}>Down</Text>
+            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueDown} onValueChange={(value) => this.setState({sliderValueDown: value})} />
 
-            <Text style={styles.text}>Y1</Text>
-            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueY1} onValueChange={(value) => this.setState({sliderValueY1: value})} />
+            <Text style={styles.text}>Forward</Text>
+            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueForward} onValueChange={(value) => this.setState({sliderValueForward: value})} />
 
-            <Text style={styles.text}>Y2</Text>
-            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueY2} onValueChange={(value) => this.setState({sliderValueY2: value})} />
+            <Text style={styles.text}>Backward</Text>
+            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueBackward} onValueChange={(value) => this.setState({sliderValueBackward: value})} />
 
-            <Text style={styles.text}>Z1</Text>
-            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueZ1} onValueChange={(value) => this.setState({sliderValueZ1: value})} />
+            <Text style={styles.text}>Left</Text>
+            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueLeft} onValueChange={(value) => this.setState({sliderValueLeft: value})} />
 
-            <Text style={styles.text}>Z2</Text>
-            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueZ2} onValueChange={(value) => this.setState({sliderValueZ2: value})} />
-            <Button onPress={this.toggleDisable} style={styles.btn}>Disable ({this.state.isDisabled ? "true" : "false"})</Button>
+            <Text style={styles.text}>Right</Text>
+            <Slider trackStyle={{marginTop: -4, backgroundColor: '#71ccff'}} thumbStyle={{backgroundColor: '#3d3d3d'}} minimumTrackTintColor='#1073ff' style={{width: 230}} value={this.state.sliderValueRight} onValueChange={(value) => this.setState({sliderValueRight: value})} />
+            <Button text="Disable" onPress={this.toggleDisable} style={styles.btn}>Disable ({this.state.isDisabled ? "true" : "false"})</Button>
           </Modal>
       </View>
     );
   }
-});
+};
 
 var styles = StyleSheet.create({
 
